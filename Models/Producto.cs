6563f2685978e2665c12ba3730140a74fd356827
Nicolas <nicolasdelponte2012@gmail.com>
2024-04-25 16:52:16 -0300

    CLASE 81 TERMINADA

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -191,7 +191,7 @@ namespace ProyectoApiNicolas.Models
                 //MySqlCommand.Parameters.AddWithValue("@id_categoria", nuevoProducto.id_categoria);
                 //MySqlCommand.Parameters.AddWithValue("@Stock", nuevoProducto.Stock);
 
-                //TO-DO: Hacer el stored proc.
+                //TO-DO: CLASE 81 TERMINADA
                 // Agrego los Parámetros al SPROC (OUT)
 
                 SqlParameter pariProductoID = new SqlParameter("@producto_id", SqlDbType.Int);
